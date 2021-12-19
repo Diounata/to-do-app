e6ee5145e05f24c350925992c75a7954931040db
Diounata <jonathamcordeiro42@gmail.com>
2021-12-19 12:51:19 -0300

    Chore: add add task button hover effect

--- src/Components/AddTask/index.tsx
+++ src/Components/AddTask/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from './styles.module.scss';
 
 import { Input } from '../Input';
@@ -5,7 +6,9 @@ import { Input } from '../Input';
 export function AddTask() {
   return (
     <section className={styles.addTaskContainer}>
-      <div></div>
+      <div>
+        <Image src="/icon-check.svg" width="10px" height="10px" alt="Add" />
+      </div>
 
       <Input placeholder="Create a new todo..." />
     </section>
